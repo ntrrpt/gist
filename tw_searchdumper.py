712212dd987a2760316c97f63a313e53f2e70ad5
nt <>
2024-07-23 18:36:56 +0300

    update searchdumper

--- tw_searchdumper.py
+++ tw_searchdumper.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 from __future__ import print_function
-import time, sys, json, subprocess, tempfile, os, pathlib, optparse
+import time, sys, json, subprocess, tempfile, os, pathlib, optparse, psutil, random, string, pause
 from datetime import datetime, timedelta
 from twikit import Client
 from loguru import logger
 
 # search_tweet => 1 search in 18-20 seconds, 50 searches in 15 minutes
-SEARCH_TWEET_DELAY = 10
-RATE_LIMIT_DELAY = 60 * 2
+SEARCH_TWEET_DELAY = 30
+TIMEOUT_DELAY = 60 * 2
 DROP_SEARCH_AFTER_X_ATTEMPTS = 1
+ARIA2_FILENAME = ''.join(random.choice(string.ascii_letters) for x in range(10)) + '.txt'
+MAX_THREADS = 2
 
 parser = optparse.OptionParser()
 parser.add_option('-s', dest='search',   default='lucky star', help='search string')
@@ -19,21 +21,14 @@ options, arguments = parser.parse_args()
 
 aria2c_args = [
     'aria2c',
-    '--input-file=tmp.txt',
+    f'--input-file={ARIA2_FILENAME}',
     f'--dir={options.search}',
-    '--max-connection-per-server=2',
+    '--max-connection-per-server=1',
     '--auto-file-renaming=false',
     '--remote-time=true',
     '--log-level=error',
     '--console-log-level=error',
     '--download-result=hide'
-]
-
-day_sequences = [
-    ['1,15'],
-    ['1,10,20'],
-    ['1,7,14,21,28'],
-    ['1,5,10,15,20,25']
 ]
 
 logger.remove(0)
@@ -43,10 +38,14 @@ logger.add(
     colorize = True, level = 5
 )
 
-def msg(*a, **ka):
-    if a:
-        a = [a[0]] + list(a[1:])
-    print(*a, **ka, end='\r')
+def childCount():
+    current_process = psutil.Process()
+    children = current_process.children()
+    return(len(children))
+
+def fileDel(filename):
+    rem_file = pathlib.Path(filename)
+    rem_file.unlink(missing_ok=True)
 
 def add(dir, bin):
     with open(dir, 'a', encoding='utf-8') as file:
@@ -80,7 +79,10 @@ def picsdump(all_tweets):
     dubs = 0
 
     for tw in all_tweets:
-        if 'type' in tw.media[0] and 'video' in tw.media[0]['type']: # video
+        if not tw.media:
+            continue
+
+        elif 'type' in tw.media[0] and 'video' in tw.media[0]['type']: # video
             url = tw.media[0]['video_info']['variants'][-1]['url']
             if url.find('?') != -1:
                 url = url[:url.find('?')] # foo.mp4?bar=12 => foo.mp4
@@ -101,22 +103,27 @@ def picsdump(all_tweets):
     if not images:
         return '' # only dubs
 
-    rem_file = pathlib.Path("tmp.txt")
-    rem_file.unlink(missing_ok=True)
+    fileDel(ARIA2_FILENAME)
 
     # aria2c download list
     for img in images:
-        add('tmp.txt', f'{img[0]}\n    out={img[1]} {os.path.basename(img[0])}')
+        add(
+            ARIA2_FILENAME, 
+            f'{img[0]}\n    out={img[1]} {os.path.basename(img[0])}'
+        )
 
     subprocess.Popen(aria2c_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-    ret_str = f'{len(images)} new'
+    ret_str = f'+{len(images)}'
     if dubs:
-        ret_str += f', {dubs} dubs'
+        ret_str += f' ~{dubs}'
 
-    return f'({ret_str})'
+    return f'{ret_str}'
 
 def searchdump(search_str):
+    def status(str):
+        print(str, end=' ', flush=True)
+
     tweets = None
     all_tweets = []
     zero_searches = 0
@@ -126,43 +133,52 @@ def searchdump(search_str):
             try:
                 tweets = client.search_tweet(search_str, 'Media') if first_search else tweets.next()
                 zero_searches = (zero_searches + 1) if not tweets else 0
-                msg('tweets len =>', len(tweets))
+                status(len(tweets))
                 all_tweets += list(tweets)
 
                 if zero_searches >= DROP_SEARCH_AFTER_X_ATTEMPTS:
                     break
 
-                time.sleep(SEARCH_TWEET_DELAY)
+                time.sleep(SEARCH_TWEET_DELAY + random.randint(-10,10))
 
                 if first_search:
                     break
 
             except Exception as ex:
                 if con(['Rate limit'], ex):
-                    msg('rate limited       ')
-                    time.sleep(RATE_LIMIT_DELAY)
+                    status('r')
+                    pause.until(ex.rate_limit_reset + 5)
 
-                elif con(['timed out', 'items'], ex): # no pics in first search
-                    logger.warn(f'{search_str} (no pics)')
-                    time.sleep(SEARCH_TWEET_DELAY)
+                elif con(['timed out', 'getaddrinfo'], ex): # rip internet
+                    status('t')
+                    time.sleep(TIMEOUT_DELAY)
+                    
+                elif con(['items'], ex): # no pics in first search
                     return
 
                 elif con(['moduleItems'], ex): # no pics in second search
                     break
 
-                elif con(['views', 'is_translatable', 'legacy'], ex): # idk
-                    time.sleep(RATE_LIMIT_DELAY)
+                elif con([
+                'list index out of range', 
+                'views', 
+                'is_translatable', 
+                'legacy',
+                'Multiple cookies exist with name',
+                'object has no attribute'], ex): # idk
+                    time.sleep(TIMEOUT_DELAY)
                     break
 
                 else:
                     logger.error(f'tw exc => {str(ex)}')
                     sys.exit()
 
-    logger.success(f'{search_str} {picsdump(all_tweets)}')
+    print('', end='\r', flush=True)
+    logger.success(f'{search_str} {picsdump(all_tweets)}        ')
     return True
 
 stop = False
-all_urls = list()
+all_urls = []
 client = rqst_client()
 
 _from, _to = options.years.split('-')
@@ -181,6 +197,9 @@ for y in years:
 
         for d in days:
             if int(d) < 10: d = '0' + str(d)
+            
+            while childCount() >= MAX_THREADS:
+                time.sleep(1)
 
             if stop or not searchdump( f'{options.search} lang:ja until:20{y}-{m}-{d}'):
                 break
@@ -188,4 +207,8 @@ for y in years:
             if datetime(int(f'20{y}'), int(m), int(d)) > datetime.now():
                 stop = True
 
-logger.info(f'all_urls => {len(all_urls)}')
+while childCount() > 0:
+    time.sleep(1)
+else:
+    logger.info(f'all_urls => {len(all_urls)}')
+    fileDel(ARIA2_FILENAME)
